700bf334e01e40f9a0e2f939b4a6b3522c4e645f
Vmakar <vasyl.makar@lembergsolutions.com>
2023-02-09 00:53:47 +0200

    init project

---
+++ factorx/src/main/java/com/makar/factorx/FactorxApplication.java
@@ -0,0 +1,13 @@
+package com.makar.factorx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorxApplication.class, args);
+	}
+
+}
